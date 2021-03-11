3a970553c109f2db7c4dd5253f994d4f9f3d34fd
growinghsb <seungbeombm@gmail.com>
2021-03-11 17:39:27 +0900

    service 및 service test complite

--- src/main/java/winfly/borad_2/service/ContentsService.java
+++ src/main/java/winfly/borad_2/service/ContentsService.java
@@ -1,17 +1,14 @@
 package winfly.borad_2.service;
 
-import winfly.borad_2.controller.dto.ContentsDto;
-import winfly.borad_2.domain.Contents;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import winfly.borad_2.controller.dto.ContentsDto;
+import winfly.borad_2.domain.Contents;
 import winfly.borad_2.repository.ContentsRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static java.time.LocalDateTime.*;
-import static java.time.format.DateTimeFormatter.*;
 
 @Service
 @Transactional
